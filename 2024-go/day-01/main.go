a7adb24124dc23108ce9e5a4fde48451bb1609b1
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-12-02 20:15:53 +0000

    Seperated input columns into slices.

--- 2024-go/day-01/main.go
+++ 2024-go/day-01/main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	input, err := os.ReadFile("input.txt")
+	input, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(string(input))
+	// Create a scanner to read the file
+	scanner := bufio.NewScanner(input)
+
+	left_slice := []int{}
+	right_slice := []int{}
+
+	// Read the file line by line
+	for scanner.Scan() {
+		// Split the line into columns
+		columns := strings.Fields(scanner.Text())
+
+		// Convert column string to int and append to right and left slices
+		i, _ := strconv.Atoi(columns[0])
+		left_slice = append(left_slice, i)
+
+		i, _ = strconv.Atoi(columns[1])
+		right_slice = append(right_slice, i)
+	}
+
+	fmt.Println(left_slice[1])
+
+	fmt.Println(right_slice[1])
 }
