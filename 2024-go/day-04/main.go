a406a8a73893ab17bf9188e744fb7495a31e2858
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-12-05 21:19:07 +0000

    Completed part 2.

--- 2024-go/day-04/main.go
+++ 2024-go/day-04/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -38,7 +39,7 @@ func part1() {
 	index_length_y := len(matrix)
 
 	// Find the 'X' character in each line
-	// In each direction, if 'X' >= 3 places away from the edge of the matrix try to mind 'M', 'A' & 'S' in the linear plane
+	// In each direction, if 'X' >= 3 places away from the edge of the matrix try to find 'M', 'A' & 'S' in the linear plane
 	for line_index, l := range matrix {
 		// Iterate through each character in the line to find each 'X' character
 		for char_index, c := range l {
@@ -76,5 +77,59 @@ func part1() {
 			}
 		}
 	}
-	fmt.Println("Count: ", count)
+	fmt.Println("XMAS count: ", count)
+}
+
+func part2() {
+	input, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create a scanner to read the file
+	scanner := bufio.NewScanner(input)
+
+	count := 0
+
+	// Create a 2D slice to store each line of letters in a slice
+	matrix := [][]string{}
+
+	// Read the file line by line and split the characters into a slice
+	for scanner.Scan() {
+		letter_split := strings.Split(scanner.Text(), "")
+
+		// Append the split letters to a slice in the matrix to represent a row
+		matrix = append(matrix, letter_split)
+	}
+
+	// Calculate the width and height of the matrix
+	index_length_x := len(matrix[0])
+	index_length_y := len(matrix)
+
+	// Iterate through each line of the matrix
+	for line_index, l := range matrix {
+		// Iterate through each character in the line to find each 'A' character
+		for char_index, c := range l {
+			// If the character 'A' is at least 1 character within the matrix boundaries then check for 2 'MAS' in the shape of an X
+			if c == "A" && line_index >= 1 && line_index <= (index_length_y-2) && char_index >= 1 && char_index <= (index_length_x-2) {
+				// Find 'MAS' in the upwards right and downwards right planes
+				if matrix[line_index-1][char_index+1] == "S" && matrix[line_index+1][char_index+1] == "S" && matrix[line_index+1][char_index-1] == "M" && matrix[line_index-1][char_index-1] == "M" {
+					count += 1
+				}
+				// Find 'MAS' in the upwards left plane and upwards right planes
+				if matrix[line_index-1][char_index-1] == "S" && matrix[line_index-1][char_index+1] == "S" && matrix[line_index+1][char_index+1] == "M" && matrix[line_index+1][char_index-1] == "M" {
+					count += 1
+				}
+				// Find 'MAS' in the downwards right plane and downwards left planes
+				if matrix[line_index-1][char_index-1] == "M" && matrix[line_index-1][char_index+1] == "M" && matrix[line_index+1][char_index+1] == "S" && matrix[line_index+1][char_index-1] == "S" {
+					count += 1
+				}
+				// Find 'MAS' in the downwards left plane and upwards left planes
+				if matrix[line_index-1][char_index+1] == "M" && matrix[line_index+1][char_index+1] == "M" && matrix[line_index+1][char_index-1] == "S" && matrix[line_index-1][char_index-1] == "S" {
+					count += 1
+				}
+			}
+		}
+	}
+	fmt.Println("X-MAS count: ", count)
 }
