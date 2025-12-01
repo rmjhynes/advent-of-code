22f33f906c3eaa5948db710b8912db634a0f1591
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-12-01 18:39:03 +0000

    feat: finish part 1

--- 2025-js/day-01/day01.js
+++ 2025-js/day-01/day01.js
@@ -4,61 +4,59 @@ function part_1() {
   const data = fs.readFileSync("input.txt", "utf8");
 
   // Split into lines (drops the trailing newline)
-  // const rotations = data.trim().split("\n");
-  //
-  // console.log(rotations);
-  //
-  // const dial_nums = Array.apply(null, Array(100)).map(function(x, i) { return i; });
-  // console.log("Dial numbers: ", dial_nums);
+  const rotations = data.trim().split("\n");
 
   let pointer = 50;
-
-  // rotations = ["L2", "R7", "L55", "R3", "L15", "R12", "R67", "R40", "L7"]
-   rotations = ["L1", "L29", "R31", "L43"]
 
   for (let i = 0; i < rotations.length; i++) {
     console.log("");
     console.log("Pointer at: ", pointer);
-    direction = rotations[i].charAt(0);
+    
+    // Direction is the first character of the rotation
+    let direction = rotations[i].charAt(0);
     console.log("Direction: ", direction);
+
     // Points is the substring after the first character
-    points = Number(rotations[i].substring(1))
+    let points = Number(rotations[i].substring(1));
+    // If > 100, dismiss the first digit as a x00 move will always result in the
+    // same point shift to the left or right
+    if (points >= 100) {
+      points = Number(rotations[i].substring(2));
+    }
+
     console.log("Points: ", points);
 
     if (direction.trim() === "L"){
       if (pointer - points < 0) {
-        // Have to account for the extra 1 shift from 0 to 99
-        // pointer = 99 - (points - points_to_zero)
-        pointer = 100 - (points - pointer)
-        check_zero(pointer);
-        console.log("Pointer now at: ", pointer)
+        pointer = 100 - (points - pointer);
       } else {
-        pointer = pointer - points
-        check_zero(pointer);
-        console.log("Pointer now at: ", pointer)
+        pointer -= points;
       }
     } else if (direction.trim() === "R"){
       if (pointer + points > 99) {
-        points_to_end = 99 - pointer
-        // points_to_end = 100 - pointer
-        pointer = (points - points_to_end - 1)
-        check_zero(pointer);
-        console.log("Pointer now at: ", pointer)
+        let points_to_end;
+
+        if (pointer === 99) {
+          points_to_end = 0;
+        } else {
+          points_to_end = 99 - pointer;
+        }
+        pointer = (points - points_to_end - 1);
       } else {
-        pointer = pointer + points
-        check_zero(pointer);
-        console.log("Pointer now at: ", pointer)
+        pointer += points;
       }
     }
-
+    check_zero(pointer);
+    console.log("Pointer now at: ", pointer);
+    console.log("Occurrences of zero so far: ", total);
   }
-
-  console.log("Total occurrences of zero: ", total);
+  console.log("");
+  console.log("Total occurrences of zero (password to the door): ", total);
 }
 
 function check_zero(pointer) {
   if (pointer === 0) {
-    total += 1
+    total += 1;
   }
 }
 
