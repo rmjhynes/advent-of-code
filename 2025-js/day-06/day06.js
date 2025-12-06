cb7bb312676ec7ebf4502e7fe64e35cf2cf1b118
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-12-06 12:54:23 +0000

    feat: finish day 6 part 1

--- 2025-js/day-06/day06.js
+++ 2025-js/day-06/day06.js
@@ -4,7 +4,6 @@ function part_1() {
   const lines = read_input();
   
   let total = 0;
-  let line_array = [];
   let elements_array = [];
 
   // Iterate over lines and process them into an array of arrays
@@ -16,22 +15,25 @@ function part_1() {
   console.log("elements_array", elements_array);
 
   // Iterate over horizontal lines
-  for (let i = 0; i < elements_array.length; i++) {
+  for (let i = 0; i < elements_array[0].length; i++) {
     // Identify the operator for the vertical line
     const operator = elements_array[elements_array.length - 1][i];
+
+    // Parse vertical elements and add to new array
     let vertical_items = elements_array.map(arr => arr[i]);
     // Remove the operator from the array
     vertical_items.splice(vertical_items.length - 1, 1);
     console.log(vertical_items);
+
     if (operator === "+") {
       let sum = 0;
       vertical_items.map(Number).map(e => sum += e);
       console.log(sum);
       total += sum;
     } else if (operator === "*") {
-        product = vertical_items.reduce((accumulator, current_value) => accumulator * current_value, 1);
-        console.log(product);
-        total += product;
+      const product = vertical_items.reduce((accumulator, current_value) => accumulator * current_value, 1);
+      console.log(product);
+      total += product;
     }
   }
   console.log("Total: ", total);
